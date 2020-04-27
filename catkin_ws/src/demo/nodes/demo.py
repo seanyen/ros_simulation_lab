1f2a2c0bdaff2214e8d80581c4056d9c03260a81
Sean Yen <seanyen@microsoft.com>
2020-04-27 16:50:45 -0700

    Trying to wait for 3 loops.

--- catkin_ws/src/demo/nodes/demo.py
+++ catkin_ws/src/demo/nodes/demo.py
@@ -13,7 +13,7 @@ NAME = 'lap_time_test'
 class TesterClass(unittest.TestCase):
     
     def __init__(self, *args):
-        self.message_received = False
+        self.message_received = 0
         self.message_received_raw = None
         rospy.init_node(NAME, anonymous=True)
         super(TesterClass, self).__init__(*args)
@@ -21,13 +21,13 @@ class TesterClass(unittest.TestCase):
     def test_passed(self): # test names must start with 'test_'
         sub = rospy.Subscriber('/lap_time', Duration, self.callback)
 
-        while not self.message_received:
+        while self.message_received < 3:
             time.sleep(0.1)
 
         self.assertTrue(rospy.Duration(30) > self.message_received_raw)
 
     def callback(self, msg):
-        self.message_received = True
+        self.message_received += 1
         self.message_received_raw = msg.data
 
 if __name__ == '__main__':
